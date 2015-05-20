d7e436112faaeac6589a6a679fb04f94b5f596e2
Atanaska <a_ivancheva97@abv.bg>
2015-05-20 23:13:24 +0300

    Removing files

--- config/initializers/omniauth_twitter.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.config.twitter_key, Rails.application.config.twitter_secret, use_authorize: true
- provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], :scope => 'user_about_me,publish_actions'
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-TwitterExample::Application.config.session_store :cookie_store, key: '_TwitterExample_session'
